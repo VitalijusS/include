215d4babc610e116cad1a9bef839f7c13bdba19b
VitalijusS <v.stifanovicius@gmail.com>
2024-11-19 16:48:52 +0200

    .

--- include.php
+++ include.php
@@ -26,7 +26,19 @@ function e($value) {
 </form>
 
 <?php
-    echo file_get_contents("pages/{$_GET['page']}.html");
+    $pages = [
+        'citrus_salmon',
+        'mediterranian_pasta',
+        'sunset_risotto',
+        'tropical_tacos'
+    ];
+
+    if (!empty($_GET['page'])) {
+        $page = $_GET['page'];
+        if (in_array($page, $pages)) {
+            echo file_get_contents("pages/{$page}.html");
+        }
+    }
 ?>
 
 
